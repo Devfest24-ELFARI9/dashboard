0e4f4a37193734993eb59d951279dc9e3ddd56b0
RayanZaki <rayan.hassici@ensia.edu.dz>
2024-10-18 11:23:42 +0100

    fixed form validation of password

--- src/lib/form.tsx
+++ src/lib/form.tsx
@@ -10,6 +10,16 @@ const Form = ({
   action: string;
 }) => {
   const router = useRouter();
+
+  const handleValidatePassword = (password: string, confirm_password: string) => {
+    console.log("password", password, "confirm_password", confirm_password);
+    if (password !== confirm_password) {
+      return false;
+    }
+    return true;
+  }
+
+
   return (
     <form
       action={action}
@@ -18,13 +28,24 @@ const Form = ({
         e.preventDefault();
         console.log("submitting form");
         const formData = new FormData(e.currentTarget);
+
+
+        if(action == '/api/signup'){
+          const isValid  = handleValidatePassword(formData.get("password") as string, formData.get("confirm_password") as string);
+          if (!isValid) {
+                  alert(
+                    `Passwords do not match`,
+                  );
+            return;
+          }
+        }
+
         const response = await fetch(action, {
           method: "POST",
           body: formData,
           // redirect: "manual",
         });
 
-        console.log("response", response);  
         if (response.ok) {
           // redirected
           // when using `redirect: "manual"`, response status 0 is returned
