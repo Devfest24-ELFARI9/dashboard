b663a0c139cee13135ecf9ac52fa3b813af54cd5
amine <la_mougari@esi.dz>
2024-10-19 20:39:47 +0100

    some ui

--- src/components/MachineCounts/MachineCountsCards.tsx
+++ src/components/MachineCounts/MachineCountsCards.tsx
@@ -2,6 +2,8 @@
 import CardWithPie, { CardWithPieProps } from "./CardWithPie";
 import React from 'react'
 
+
+
 export const data: CardWithPieProps[] = [
   {
     title: "Stamping Presses",
@@ -16,26 +18,26 @@ export const data: CardWithPieProps[] = [
     down_count: 3,
   },
   {
-    title: "Stamping Presses",
-    machine_id: "stamping_press_001",
+    title: "CNC Milling Machine",
+    machine_id: "cnc_milling_004",
     working_count: 4,
     down_count: 1,
   },
   {
-    title: "Welding Robots",
-    machine_id: "welding_robot_006",
+    title: "Automated Guided Vehicle",
+    machine_id: "agv_003",
     working_count: 1,
     down_count: 3,
   },
   {
-    title: "Stamping Presses",
-    machine_id: "stamping_press_001",
+    title: "Painting Robots",
+    machine_id: "painting_robot_002",
     working_count: 4,
     down_count: 1,
   },
   {
-    title: "Welding Robots",
-    machine_id: "welding_robot_006",
+    title: "Leak Test Machine",
+    machine_id: "leak_test_005",
     working_count: 1,
     down_count: 3,
   },
